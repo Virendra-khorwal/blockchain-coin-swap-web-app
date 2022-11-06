c4025583e7dd72ce44480eaaf9d74e7b217ab679
Virendra Khorwal <virendrakhorwalvk@gmail.com>
2022-11-07 01:25:18 +0530

    minor fix

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -4,7 +4,7 @@ import { AiFillSignal, AiOutlineAreaChart, BsBack } from "react-icons/ai";
 import { IoIosWallet, IoMdSettings } from "react-icons/io";
 import { IoRibbon, IoLogInOutline, IoLogOutOutline } from "react-icons/io5";
 
-import logo from "../public/logo.png";
+import logo from "../asset/logo.svg";
 import Image from "next/image";
 import { useRouter } from "next/router";
 
@@ -15,10 +15,10 @@ const Sidebar = () => {
   return (
     <div className="h-screen bg-dark-secondary text-white flex flex-col justify-between items-center py-6 w-32 sm:w-16">
       <div className="cursor-pointer">
-        <Image src={logo} alt="Logo" />
+        <Image width={60} height={60} src={logo} alt="Logo" />
       </div>
       <div className="text-3xl sm:text-sm">
-        <ul className="flex flex-col gap-y-10 sm:gap-y-2 ">
+        <ul className="flex flex-col gap-y-4 sm:gap-y-2 ">
           <li>
             <Link href="/">
               <div
